c89cc76200a26911eec863591579f00f65eb382b
murugesanopenssl <145038227+murugesanopenssl@users.noreply.github.com>
2025-12-02 19:45:18 +0530

    Add files via upload

--- text.js
+++ text.js
@@ -4,54 +4,51 @@ function writeMenu()
 	document.write(`
 			<TABLE border='1' style='width:100%;height=30px;'>
 				<TR>`);
-	if( -1 == location_url.indexOf("index.html") && -1 != location_url.indexOf("NMurugesan_cv") )
+	if( -1 !== location_url.indexOf("index.html") && -1 === location_url.indexOf("poems") )
 	{
-		if( -1 != location_url.indexOf("NMurugesan_cv") )
-		{
-			document.write( `<TD style="text-align:right;vertical-align:top;" bgcolor='orange'>`);
-		}
-		else
-		{
-			document.write( `<TD style="text-align:right;vertical-align:top;">`);
-		}
+		document.write(`<TD style="display:flex; justify-content:flex-end; align-items:flex-start; background:orange;">`);
+	}
+	else if(
+		-1 !== location_url.indexOf("NMurugesan_cv")	||
+		-1 !== location_url.indexOf("openssl_speed")	
+	)
+	{
+		document.write(`<TD style="text-align:right;vertical-align:top;" bgcolor='orange'>`);
 	}
 	else
 	{
-		document.write( `<TD style="text-align:right;vertical-align:top;" bgcolor='orange'>`);
+		document.write(`<TD style="text-align:right;vertical-align:top;">`);
 	}
-	document.write( `<DIV class="menu">`);
-	if( -1 == location_url.indexOf("index.html") )
+	if( -1 !== location_url.indexOf("index.html") && -1 === location_url.indexOf("poems") )
 	{
-		document.write(	`<A href='index.html'>Home12</A>`);
+		document.write( `<DIV class="menu" style="text-align:right; vertical-align:top;">`);
+		document.write(`Home`);
 	}
 	else
 	{
-		document.write(	"Home12");
-		var submenu = document.querySelector('.submenu'); // Assuming there's a div with class 'submenu'
-		if (submenu) {
-			submenu.innerHTML = 'Home2'; // Add 'Home2' inside the submenu div
-		}
+		document.write( `<DIV class="menu" style="text-align:right; vertical-align:top;">`);
+		document.write(`<A href='index.html'>Home</A>`);
 	}
 	if( -1 != location_url.indexOf("NMurugesan_cv") )
 	{
-		document.write(		`<DIV class="submenu no-link">Murugesan N CV`);
+		document.write(		`<DIV class="submenu no-link">Murugesan2 N CV&nbsp;`);
 	}
 	else
 	{
-		document.write(		`<DIV class="submenu"><A href="NMurugesan_cv.html">Murugesan N CV</A>`);
+		document.write(		`<DIV class="submenu"><A href="NMurugesan_cv.html">Murugesan3 N CV&nbsp;</A>`);
 	}
 	if( -1 != location_url.indexOf("openssl_speed") )
 	{
-		document.write(		`<DIV class="submenu no-link">OpenSSL Speed`);
+		document.write(		`<DIV class="submenu no-link">OpenSSL Speed&nbsp;`);
 	}
 	else
 	{
-		document.write(		`<DIV class="submenu"><A href="openssl_speed.html">OpenSSL Speed</A>`);
+		document.write(		`<DIV class="submenu"><A href="openssl_speed.html">OpenSSL Speed&nbsp;</A>`);
 	}
 	document.write(		`</DIV>
 						</DIV>
 					</TD>`);
-	if( -1 == location_url.indexOf("spending.html") && -1 == location_url.indexOf("displayMoneyHelpMenu") )
+	if( -1 == location_url.indexOf("displayMoneyHelpMenu") )
 	{
 		document.write(	`<TD style="text-align:right;vertical-align:top;">`);
 	}
@@ -60,7 +57,7 @@ function writeMenu()
 		document.write(	`<TD style="background-color:orange;text-align:right;vertical-align:top;">`);
 	}
 	document.write(		`<DIV class="menu">`);
-	if( -1 == location_url.indexOf("spending.html") && -1 == location_url.indexOf("displayMoneyHelpMenu") )
+	if( -1 == location_url.indexOf("displayMoneyHelpMenu") )
 	{
 		document.write(			`<A href='displayMoneyHelpMenu.html'>Moneyhelp1</A>`);
 	}
@@ -78,53 +75,117 @@ function writeMenu()
 	document.write(				`<DIV class="submenu">
 								<A href="displayMoneyHelpMenu.html">Financial Resource</A>
 								<A href="displayMoneyHelpMenu.html">Government Resource</A>`);
-	let currentPage = window.location.pathname.split("/").pop();  // gets "index.html" or "spending.html"
+	let currentPage = window.location.pathname.split("/").pop();
 	if( "spending.html" !== currentPage )
 	{
-		document.write(			`<A href="spending.html" onclick='toggleTable(event);'>Save Money Daily</A>`);
+		// document.write(			`<A href="spending.html" onclick='toggleTable(event);'>Save1 Money Daily</A>`);
 	}
 	else
 	{
 		// document.write( `<A onclick='toggleTable(event);' style='display:block;'><B><U><FONT color='red'>Save1 Money Daily</B></U></FONT>`);
-		document.write( `<A href='spending.html' onclick="toggleTable(event)" id="PreventWastagehref">Save Money Daily</A>`);
+		// document.write( `<A href='spending.html' onclick="toggleTable(event)" id="PreventWastagehref">Save2 Money Daily</A>`);
 	}
 	document.write(			`</DIV>
 						</DIV>
-					</TD>
-					<TD style="text-align:right;vertical-align:top;">
-						<DIV class="menu">
-							<A href='ShareKnowledgeTransferToPeople.html'>Share knowledge transfer</A>
-							<DIV class="submenu">
-								<A href="NMurugesan_cv.html">Murugesan N CV</A>
-								<A href="openssl_speed.html">OpenSSL Speed</A>
-							</DIV>
-						</DIV>
-					</TD>
-					<TD style="text-align:right;vertical-align:top;">
-						<DIV class="menu">
-							<A href="remove-blockages-kitchen-pvc-outflow-pipe.html">Unclog Kitchen Drain</A>
-							<DIV class="submenu">
-								<A href="openssl_speed.html">OpenSSL Speed</A>
-							</DIV>
-						</DIV>
-					</TD>
-					<TD style="text-align:right;vertical-align:top;">
-						<DIV class="menu">
-							<A href="Visitor_count.html">Visitor count</A>
-						</DIV>
-					</TD>
-					<TD style="text-align:right;vertical-align:top;">
-						<DIV class="menu">
-							<A href="tamil-calendar.html">Tamil/Gregorian calendar</A>
-						</DIV>
 					</TD>`);
+	if( -1 !== location_url.indexOf( "spending" ) )
+	{
+		document.write(			`<TD style="text-align:right;vertical-align:top;background-color:orange;">`);
+		document.write(		`<A onclick="toggleTable(event)" id="PreventWastagehref" style='text-color:white;'>Save Money Daily</A>`);
+	}
+	else
+	{
+		document.write(			`<TD style="text-align:right;vertical-align:top;">`);
+		document.write(		`<A href='spending.html' onclick="toggleTable(event)" id="PreventWastagehref">Save Money Daily</A>`);
+	}
+	document.write(		`</TD>`);
+	if( -1 !== location_url.indexOf( "ShareKnowledgeTransferToPeople" ) )
+	{
+		document.write(		`<TD style="text-align:right;vertical-align:top;background-color:orange;">`);
+	}
+	else
+	{
+		document.write(		`<TD style="text-align:right;vertical-align:top;">`);
+	}
+	if( -1 !== location_url.indexOf("ShareKnowledgeTransferToPeople") )
+	{
+		document.write(`<DIV class="menu active-menu"><DIV class="submenu no-link">Share knowledge transfer</DIV></DIV>`);
+	}
+	else
+	{
+		document.write(`<DIV class="menu"><A href="ShareKnowledgeTransferToPeople.html">Share knowledge transfer</A></DIV>`);
+	}
+	document.write(		`</DIV>
+					</TD>`);
+	if( -1 !== location_url.indexOf("remove-blockages-kitchen-pvc-outflow-pipe") )
+	{
+		document.write(`
+			<TD style="text-align:right;vertical-align:top;background-color:orange;">
+				<DIV class="menu active-menu">
+					<DIV class="submenu no-link">Unclog Kitchen Drain</DIV>
+				</DIV>
+			</TD>
+		`);
+	}
+	else
+	{
+		document.write(`
+			<TD style="text-align:right;vertical-align:top;">
+				<DIV class="menu">
+					<A href="remove-blockages-kitchen-pvc-outflow-pipe.html">Unclog Kitchen Drain</A>
+				</DIV>
+			</TD>
+		`);
+	}
+	if( -1 !== location_url.indexOf("Visitor_count") )
+	{
+		document.write(`
+			<TD style="text-align:right;vertical-align:top;background-color:orange;">
+				<DIV class="menu active-menu">
+					<DIV class="submenu no-link">Visitor count</DIV>
+				</DIV>
+			</TD>
+		`);
+	}
+	else
+	{
+		document.write(`
+			<TD style="text-align:right;vertical-align:top;">
+				<DIV class="menu">
+					<A href="Visitor_count.html">Visitor count</A>
+				</DIV>
+			</TD>
+		`);
+	}
+	if( -1 !== location_url.indexOf("tamil-calendar") )
+	{
+		// Active page version
+		document.write(`
+			<TD style="text-align:right;vertical-align:top;background-color:orange;">
+				<DIV class="menu active-menu">
+					<DIV class="submenu no-link">Tamil/Gregorian calendar</DIV>
+				</DIV>
+			</TD>
+		`);
+	}
+	else
+	{
+		// Normal link version
+		document.write(`
+			<TD style="text-align:right;vertical-align:top;">
+				<DIV class="menu">
+					<A href="tamil-calendar.html">Tamil/Gregorian calendar</A>
+				</DIV>
+			</TD>
+		`);
+	}
 		if( -1 !== location_url.indexOf("my_exercise") )
 		{
-			document.write( `<TD style="background-color:orange;text-align:right;vertical-align:top;">`);
+			document.write( `<TD style="display:flex; justify-content:flex-end; align-items:flex-start; background:orange;">`);
 		}
 		else
 		{
-			document.write( `<TD style="text-align:right;vertical-align:top;">`);
+			document.write( `<TD style="display:flex; justify-content:flex-end; align-items:flex-start;">`);
 		}
 		document.write( `	<DIV class="">`);
 		if( -1 !== location_url.indexOf("my_exercise") )
@@ -135,20 +196,62 @@ function writeMenu()
 		{
 			document.write( `<A href='my_exercise.html'>My exercise</A>`);
 		}
-		document.write( `</DIV>
-					</TD>
-					<TD style="text-align:right;vertical-align:top;">
-						<DIV class="menu">
-							<A href="poems/index.html">My tamil poems</A>
-							<DIV class="submenu">
-								<A href="poems/1.html">My poem 01</A>
-								<A href="poems/2.html">My poem 02</A>
-								<A href="poems/3.html">My poem 03</A>
-								<A href="poems/4.html">My poem 04</A>
-								<A href="poems/5.html">My poem 05</A>
+		if( -1 !== location_url.indexOf("Windows_ERR_HELP") )
+		{
+			document.write(`
+				<TD style="text-align:right;vertical-align:top;background-color:orange;">
+					<DIV class="menu active-menu">
+						<DIV class="submenu no-link">Windows_ERR_HELP</DIV>
+					</DIV>
+				</TD>
+			`);
+		}
+		else
+		{
+			document.write(`
+				<TD style="text-align:right;vertical-align:top;">
+					<DIV class="menu">
+						<A href="Windows_ERR_HELP.html">Windows_ERR_HELP</A>
+					</DIV>
+				</TD>
+			`);
+		}
+		if( -1 !== location_url.indexOf("poems") )
+		{
+			document.write( `</DIV>
+						</TD>
+						<TD style="text-align:right;vertical-align:top;">
+							<DIV class="menu">
+								<A href="poems/index.html">My tamil poems</A>
+								<DIV class="submenu">
+									<A href="1.html">My poem 01</A>
+									<A href="2.html">My poem 02</A>
+									<A href="3.html">My poem 03</A>
+									<A href="4.html">My poem 04</A>
+									<A href="5.html">My poem 05</A>
+								</DIV>
 							</DIV>
-						</DIV>
-					</TD>
-				</TR>
-			</TABLE>`);
+						</TD>
+					</TR>
+				</TABLE>`);
+		}
+		else
+		{
+			document.write( `</DIV>
+						</TD>
+						<TD style="text-align:right;vertical-align:top;">
+							<DIV class="menu">
+								<A href="poems/index.html">My tamil poems</A>
+								<DIV class="submenu">
+									<A href="poems/1.html">My poem 01</A>
+									<A href="poems/2.html">My poem 02</A>
+									<A href="poems/3.html">My poem 03</A>
+									<A href="poems/4.html">My poem 04</A>
+									<A href="poems/5.html">My poem 05</A>
+								</DIV>
+							</DIV>
+						</TD>
+					</TR>
+				</TABLE>`);
+		}
 }
