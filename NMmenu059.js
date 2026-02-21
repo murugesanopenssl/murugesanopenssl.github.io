5db9fc0f3b356c3aad4ff9a5da1a64d6bd781cc4
murugesan openssl <murugesangct@gmail.com>
2026-02-21 10:50:47 +0000

    updated

--- NMmenu059.js
+++ NMmenu059.js
@@ -1625,7 +1625,7 @@ function loadMoneyHelpContent( event, htmlFileName )
 						<A href="#" onclick="javascript:loadHomeContent( event, 'murugesan_achievements.html' );" class="nav-link">My achievements</A>
 					</LI>
 					<LI>
-						<A href="#" onclick="javascript:window.open( 'poems/index.html');" class="nav-link"><FONT color='#00ff00'>My tamil poems</FONT></A>
+						<A href="#" onclick="javascript:window.open( 'https://murugesanopenssl.github.io/poems/index.html');" class="nav-link"><FONT color='#00ff00'>My tamil poems</FONT></A>
 					</LI>
 					<LI>
 						<A href="#" onclick="javascript:loadHomeContent( event, 'Sustainable_Living.html' );" class="nav-link">Sustainable Living</A>
